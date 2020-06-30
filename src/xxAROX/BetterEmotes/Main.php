2525595b0ee8b89d8343b2337d56d9d0e0aab38b
xxAROX <57589973+xxAROX@users.noreply.github.com>
2020-06-30 17:22:11 +0200

    Add files via upload

---
+++ src/xxAROX/BetterEmotes/Main.php
@@ -0,0 +1,30 @@
+<?php
+/* Copyright (c) 2020 xxAROX. All rights reserved. */
+namespace xxAROX\BetterEmotes;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+
+/**
+ * Class Main
+ * @package xxAROX\BetterEmotes
+ * @author xxAROX
+ * @date 30.06.2020 - 17:17
+ * @project BetterEmotes
+ */
+class Main extends PluginBase implements Listener{
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onDisable(): void{
+	}
+	public function onData(\pocketmine\event\server\DataPacketReceiveEvent $event): void{
+		$pk = $event->getPacket();
+		if ($pk instanceof \pocketmine\network\mcpe\protocol\EmotePacket) {
+			var_dump($event->getPlayer()->getId());
+			var_dump($pk->getEmoteId());
+			var_dump($pk->getFlags());
+			\pocketmine\Server::getInstance()->broadcastPacket(\pocketmine\Server::getInstance()->getOnlinePlayers(), \pocketmine\network\mcpe\protocol\EmotePacket::create($event->getPlayer()->getId(), $pk->getEmoteId(), 1 << 0));
+		}
+	}
+}
